af465912e5bcb057c977c357b3a1b1c8f918413b
Victor Rafael Alves Garcia <victor_garcia@sicredi.com.br>
2019-06-01 11:24:40 -0300

    aaaaaaaaaaaaaaaaaaaaa

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/form')
+def index():
+    return render_template('form.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
